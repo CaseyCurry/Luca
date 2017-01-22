1f403af0e817bf315cbc281e126a4f5788b4788e
Casey Curry <casey.de.curry@gmail.com>
2017-01-22 14:16:01 -0600

    rename client directory in services/service-registry

--- services/service-registry/client/src/host.js
+++ services/service-registry/client/src/host.js
@@ -4,7 +4,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "app")));
 
 const port = 12000;
 app.listen(port, () => {
