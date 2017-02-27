c865bdf1fe3cedf36accc5804882062d26f1fee2
Casey Curry <casey.de.curry@gmail.com>
2017-02-26 18:12:58 -0600

    reorg Luca

---
+++ dev/services/users/client/src/host.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const clientInitializer = require("luca-client-initializer");
+const path = require("path");
+
+const staticFileLocation = path.join(__dirname, "app");
+clientInitializer.initialize("users-client", staticFileLocation);
---
+++ ops/start-go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+open /Applications/Go\ Server.app
+GO_APPLICATION_NAME="Go Agent 1" open /Applications/Go\ Agent\ 1.app
+#GO_APPLICATION_NAME="Go Agent 2" open /Applications/Go\ Agent\ 2.app
+#GO_APPLICATION_NAME="Go Agent 3" open /Applications/Go\ Agent\ 3.app
