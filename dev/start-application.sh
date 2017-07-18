0a0ea400f078b6a8d4142a2b571748d2b7f31c5b
Casey Curry <casey.de.curry@gmail.com>
2017-07-17 21:02:46 -0500

    fix e2e tests

--- dev/start-application.sh
+++ dev/start-application.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
 export TOKEN_SECRET=123
+[[ $1 = "--skip-build" ]] && skip_build=true || skip_build=false
 
 function start {
   app_dir=$1
   app_name=$2
-  cd $app_dir
-  ./node_modules/.bin/webpack
+  skip_build=$3
+
+  if [ $skip_build ]; then
+    ./node_modules/.bin/webpack
+  fi
+
   pm2 start $app_dir/dist/host.js --name $app_name --watch $app_dir/dist --output ~/github/Luca/dev/run-logs/$app_name.output.log --error ~/github/Luca/dev/run-logs/$app_name.error.log
 }
 
 pm2 delete all
 
-start ~/github/Luca/dev/services/service-registry/api service-registry-api
-start ~/github/Luca/dev/services/service-registry/client service-registry-client
-start ~/github/Luca/dev/services/categories/api categories-api
-start ~/github/Luca/dev/services/users/api users-api
-start ~/github/Luca/dev/services/users/client users-client
-start ~/github/Luca/dev/services/checking-account/api checking-account-api
-start ~/github/Luca/dev/services/checking-account/client checking-account-client
+start ~/github/Luca/dev/services/service-registry/api service-registry-api $skip_build
+start ~/github/Luca/dev/services/service-registry/client service-registry-client $skip_build
+start ~/github/Luca/dev/services/categories/api categories-api $skip_build
+start ~/github/Luca/dev/services/users/api users-api $skip_build
+start ~/github/Luca/dev/services/users/client users-client $skip_build
+start ~/github/Luca/dev/services/checking-account/api checking-account-api $skip_build
+start ~/github/Luca/dev/services/checking-account/client checking-account-client $skip_build
 
 sleep 1s
 
-start ~/github/Luca/dev/application application
+start ~/github/Luca/dev/application application $skip_build
